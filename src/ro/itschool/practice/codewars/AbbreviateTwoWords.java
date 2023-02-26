e8239e415d1a02ffcef5743bb9262e1bdf12aaf5
Alex Iliescu <alexandru.iliescu@fortechproducts.ro>
2023-02-26 23:02:20 +0200

    Code formatting

--- src/ro/itschool/practice/codewars/AbbreviateTwoWords.java
+++ src/ro/itschool/practice/codewars/AbbreviateTwoWords.java
@@ -8,6 +8,5 @@ public class AbbreviateTwoWords {
         String lastName = names[1].substring(0, 1).toUpperCase();
         return firstName + "." + lastName;
     }
-
 }
 //https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/java
